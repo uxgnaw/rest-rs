589b22895111d99838f99c4766ac82f64f63a79b
wangxu <wangxu1@oneniceapp.com>
2021-04-11 08:38:27 +0800

    fix name

--- src/handler/add_user.rs
+++ src/handler/add_user.rs
@@ -9,21 +9,21 @@ use crate::models;
 #[get("/add/user/{name}")]
 pub async fn add_user(name: web::Path<String>) -> Result<HttpResponse, Error> {
     // use web::block to offload blocking Diesel code without blocking server thread
-    info!("add one user {}", name);
-
-    let user = web::block(move || insert_new_user(&name))
+    let nm = name.clone();
+    let user = web::block(|| insert_new_user(nm))
         .await
         .map_err(|e| {
             eprintln!("{}", e);
             HttpResponse::InternalServerError().finish()
         })?;
 
+    info!("add user {} success", name);
     Ok(HttpResponse::Ok().json(user))
 }
 
 fn insert_new_user(
     // prevent collision with `name` column imported inside the function
-    nm: &str,
+    nm: String,
 ) -> Result<models::User, diesel::result::Error> {
     // It is common when using Diesel with Actix web to import schema-related
     // modules inside a function's scope (rather than the normal module's scope)
@@ -36,7 +36,7 @@ fn insert_new_user(
         .expect("couldn't get db connection from pool");
     let new_user = models::User {
         id: Uuid::new_v4().to_string(),
-        name: nm.to_owned(),
+        name: nm,
     };
 
     diesel::insert_into(users)
