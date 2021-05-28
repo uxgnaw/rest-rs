147671370a03e67b2d70dbd16e8472de6725c84d
wangxu <wangxu1@oneniceapp.com>
2021-05-28 17:01:35 +0800

    remove lazy_static

--- src/db.rs
+++ src/db.rs
@@ -1,20 +1,7 @@
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager, PooledConnection};
 use ::r2d2::Error as r2d2Error;
-// use lazy_static::lazy_static;
 use once_cell::sync::OnceCell;
-
-// lazy_static! {
-//     pub static ref POOL: r2d2::Pool<ConnectionManager<diesel::MysqlConnection>> = {
-//         let conn_spec = std::env::var("DATABASE_URL").expect("DATABASE_URL");
-//         let manager = ConnectionManager::<MysqlConnection>::new(conn_spec);
-//         r2d2::Pool::builder()
-//             .max_size(200)
-//             .min_idle(Some(10))
-//             .build(manager)
-//             .expect("Failed to create pool.")
-//     };
-// }
 
 static POOL: OnceCell<r2d2::Pool<ConnectionManager<diesel::MysqlConnection>>> = OnceCell::new();
 
